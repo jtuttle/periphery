b3829072cec0cdec18f77fa00d38a207c270128e
Max <magicmaxmark+10@gmail.com>
2019-01-26 12:56:11 -0500

    added bttery, code doesnt work

---
+++ Assets/Scripts/BatteryMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryMove : MonoBehaviour
+{
+    public GameObject Player;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("foo");
+        gameObject.transform.parent = Player.transform;
+    }
+ 
+}
