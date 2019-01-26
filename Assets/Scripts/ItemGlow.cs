2c0cc3e7257750119c1b6c6b0203675f91d6d0c3
John Tuttle <jtuttle@conjur.net>
2019-01-26 14:34:35 -0500

    organize objects into folders, glowing batterY

---
+++ Assets/Scripts/ItemGlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGlow : MonoBehaviour
+{
+    private GameObject _player;
+    private GameObject _model;
+    private float _glowThreshold = 4f;
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+        _model = gameObject.transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(_player.transform.position, gameObject.transform.position);
+        float outline = (distance < _glowThreshold) ? 0.3f : 0;
+
+        _model.GetComponent<Renderer>().material.SetFloat("_Outline", outline);
+    }
+}
