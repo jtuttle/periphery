e92e07aeab0521d5905746c96847e3cdda390558
Max <magicmaxmark+10@gmail.com>
2019-01-26 00:09:03 -0500

    started radius calulation

---
+++ Assets/Scripts/LightDie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDie : MonoBehaviour
+{
+    public Light spotLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //float lightDist = transform.;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ Assets/Scripts/LightRadius.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRadius : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Plane;
+    [HideInInspector]
+    public float radius;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float height = Mathf.Abs(transform.position.z - Plane.transform.position.z);
+        float angle = gameObject.GetComponent<Light>().spotAngle / 2;
+        radius = Mathf.Tan(Mathf.Deg2Rad*angle)*height;
+        Debug.Log("radius" + radius);
+        Debug.Log("height" + height);
+        Debug.Log("angle" + angle);
+    }
+}
