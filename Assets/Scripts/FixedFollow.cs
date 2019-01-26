8558779103f1e85b91de2981fc52e708d9cc2a99
John Tuttle <jtuttle@conjur.net>
2019-01-26 18:08:35 -0500

    laggy camera

--- Assets/Scripts/FixedFollow.cs
+++ Assets/Scripts/FixedFollow.cs
@@ -6,21 +6,22 @@ public class FixedFollow : MonoBehaviour
 {
     public GameObject Target;
 
+    public float Damping = 1;
+
     private float _zDiff;
 
-    // Start is called before the first frame update
     void Start()
     {
         _zDiff = gameObject.transform.position.z - Target.transform.position.z;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float x = Target.transform.position.x;
         float z = Target.transform.position.z + _zDiff;
 
-        gameObject.transform.position = new Vector3(x, gameObject.transform.position.y, z);
+        Vector3 desiredPosition = new Vector3(x, gameObject.transform.position.y, z);
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, desiredPosition, Time.deltaTime * Damping);
 
         gameObject.transform.LookAt(Target.transform);
     }
