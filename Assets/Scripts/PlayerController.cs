434d157642d4b336120b62dfbfd4ed7670e4196a
John Tuttle <jtuttle@conjur.net>
2019-01-25 21:39:40 -0500

    player prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    void Start ()
+    {
+        
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        float xPos = transform.position.x + (moveHorizontal * speed);
+        float zPos = transform.position.z + (moveVertical * speed);
+
+        transform.position = new Vector3(xPos, 0, zPos);
+    }
+}
