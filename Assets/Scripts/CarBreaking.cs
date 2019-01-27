b79ce76659ddd12d6ee8dd5405e71cc414ebe5d5
Max <magicmaxmark+10@gmail.com>
2019-01-26 19:47:01 -0500

    made the car breakdown

---
+++ Assets/Scripts/CarBreaking.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBreaking : MonoBehaviour
+{
+    float timer = 1f;
+    int counter = 1;
+    bool breakCar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+
+    {
+        Debug.Log("Trigered");
+        breakCar = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Untriggered");
+        breakCar = false;
+    }
+    void Update()
+    {
+        if(breakCar == true)
+        {
+            Debug.Log("BreakCar True");
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            if (timer <= 0)
+            {
+                GameObject part = gameObject.transform.Find("Part" + counter.ToString()).gameObject;
+                GameObject.Destroy(part);
+                timer = 1;
+                counter += 1;
+
+            }
+        }
+    }
+}
