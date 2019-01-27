cbd0e066cf5b71444191dc3e000f96b7630b3248
Max <magicmaxmark+10@gmail.com>
2019-01-27 09:02:09 -0500

    added sounds

---
+++ Assets/Scripts/:Users:MMark:Documents:Perifery:Assets:Scripts:SoundBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBox : MonoBehaviour
+{
+    public AudioSource spookySounds;
+    public AudioSource whiteNoise;
+    public AudioSource Music;
+    public Light spotLight;
+    public GameObject RV;
+    float spookyCounter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        whiteNoise.volume = Vector3.Distance(RV.transform.position, gameObject.transform.position);
+        spookyCounter += Time.deltaTime;
+        if (spookyCounter > (1, 100, 100, 1, Vector3.Distance(RV.transform.position, gameObject.transform.position))
+        {
+
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public AudioSource spookySounds;
+    public AudioSource whiteNoise;
+    public AudioSource Music;
+    public Light spotLight;
+    public GameObject RV;
+    float spookyCounter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        whiteNoise.volume = Vector3.Distance(RV.transform.position, gameObject.transform.position);
+        spookyCounter += Time.deltaTime;
+        if (spookyCounter > (Mathf.Lerp(100, 1, Mathf.InverseLerp(1, 100, Vector3.Distance(RV.transform.position, gameObject.transform.position)))))
+        {
+
+        }
+
+
+    }
+}
